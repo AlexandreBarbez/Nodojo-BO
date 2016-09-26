8b91158fa3a6c9867eac6311566ccbf0fe83893d
Alexandre Barbez <alexandre.barbez@gmail.com>
2016-09-26 21:49:30 +0200

    with source files

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,16 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"event"})
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
